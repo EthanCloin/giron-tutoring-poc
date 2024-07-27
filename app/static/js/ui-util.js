99413b7c87fb3fe2a399851559d23a012bfaab2c
Ethan Cloin <psnethan@gmail.com>
2024-07-27 15:01:09 -0400

    Minor Refactors:
    - put svg into a template so i don't have to look at those numbers
    - replace custom js fxn with anchor tag to redirect
    - put navbar css into the layout base template
    - use url_for fxn where possible

--- app/static/js/ui-util.js
+++ app/static/js/ui-util.js
@@ -1,31 +1,27 @@
-function redirectTo(url) {
-    window.open(url, '_blank');
-}
+document.addEventListener('DOMContentLoaded', function () {
+  // Read more
+  const readMoreBtns = document.querySelectorAll('.read-more-btn');
+  const textElements = document.querySelectorAll('.tutors-list-item-bio');
+  const previewLength = 50;
+  let fullTexts = [];
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Read more
-    const readMoreBtns = document.querySelectorAll(".read-more-btn");
-    const textElements = document.querySelectorAll(".tutors-list-item-bio");
-    const previewLength = 50;
-    let fullTexts = [];
+  textElements.forEach((element, index) => {
+    fullTexts[index] = element.getAttribute('data-full-text');
+    if (fullTexts[index].length > previewLength) {
+      const previewText = fullTexts[index].substring(0, previewLength) + '...';
+      element.textContent = previewText;
 
-    textElements.forEach((element, index) => {
-        fullTexts[index] = element.getAttribute("data-full-text");
-        if (fullTexts[index].length > previewLength) {
-            const previewText = fullTexts[index].substring(0, previewLength) + "...";
-            element.textContent = previewText;
-
-            readMoreBtns[index].addEventListener("click", function () {
-                if (element.textContent === previewText) {
-                    element.textContent = fullTexts[index];
-                    this.textContent = "Read Less";
-                } else {
-                    element.textContent = previewText;
-                    this.textContent = "Read More";
-                }
-            });
+      readMoreBtns[index].addEventListener('click', function () {
+        if (element.textContent === previewText) {
+          element.textContent = fullTexts[index];
+          this.textContent = 'Read Less';
         } else {
-            readMoreBtns[index].style.display = "none";
+          element.textContent = previewText;
+          this.textContent = 'Read More';
         }
-    });
+      });
+    } else {
+      readMoreBtns[index].style.display = 'none';
+    }
+  });
 });
